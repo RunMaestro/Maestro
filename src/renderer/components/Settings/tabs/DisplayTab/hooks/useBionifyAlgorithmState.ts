fa08295b21481e3b28c326c30120906c2bf6d7d2
Raza Rauf <reachrazarauf@gmail.com>
2026-06-25 17:06:09 -0600

    Refactor DisplayTab into focused sections

---
+++ src/renderer/components/Settings/tabs/DisplayTab/hooks/useBionifyAlgorithmState.ts
@@ -0,0 +1,37 @@
+import { useState } from 'react';
+import { DEFAULT_BIONIFY_ALGORITHM } from '../../../../../utils/bionifyReadingMode';
+import type { BionifyAlgorithmState } from '../types';
+import { isValidBionifyAlgorithm, normalizeBionifyAlgorithm } from '../utils';
+
+interface UseBionifyAlgorithmStateOptions {
+	bionifyAlgorithm: string | undefined;
+	setBionifyAlgorithm: (value: string) => void;
+}
+
+export function useBionifyAlgorithmState({
+	bionifyAlgorithm,
+	setBionifyAlgorithm,
+}: UseBionifyAlgorithmStateOptions): BionifyAlgorithmState {
+	const [algorithmDraft, setAlgorithmDraft] = useState(
+		bionifyAlgorithm ?? DEFAULT_BIONIFY_ALGORITHM
+	);
+	const [showInfoModal, setShowInfoModal] = useState(false);
+	const isAlgorithmValid = isValidBionifyAlgorithm(algorithmDraft);
+
+	const commitAlgorithmDraft = () => {
+		const normalizedDraft = normalizeBionifyAlgorithm(algorithmDraft);
+		if (isAlgorithmValid && normalizedDraft !== (bionifyAlgorithm ?? DEFAULT_BIONIFY_ALGORITHM)) {
+			setBionifyAlgorithm(normalizedDraft);
+		}
+	};
+
+	return {
+		algorithmDraft,
+		setAlgorithmDraft,
+		isAlgorithmValid,
+		commitAlgorithmDraft,
+		showInfoModal,
+		openInfoModal: () => setShowInfoModal(true),
+		closeInfoModal: () => setShowInfoModal(false),
+	};
+}
